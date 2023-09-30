b60f44bda3321a1f9f984f3eb6ad4b6f13fac4ac
Olubo Ifeoluwa <ifeoluwaolubo@gmail.com>
2023-09-30 10:58:02 +0100

    wrote tests for updating the default message

--- test/Inbox.test.js
+++ test/Inbox.test.js
@@ -6,17 +6,32 @@ const { interface, bytecode } = require("../compile");
 
 let accounts;
 let inbox;
+let initialString = "Hello Contract";
 
 beforeEach(async () => {
   accounts = await web3.eth.getAccounts();
 
   inbox = await new web3.eth.Contract(JSON.parse(interface))
-    .deploy({ data: bytecode, arguments: ["Hello Contract"] })
+    .deploy({ data: bytecode, arguments: [initialString] })
     .send({ from: accounts[0], gas: "1000000" });
 });
 
 describe("Inbox", () => {
   it("deploys a contract", () => {
     assert.ok(inbox.options.address);
+  });
+
+  it("has a default message", async () => {
+    const message = await inbox.methods.message().call();
+    assert.equal(message, initialString);
+  });
+
+  it("updates the message", async () => {
+    const newMessage = "Hello Contract World";
+    await inbox.methods.setMessage(newMessage).send({ from: accounts[0] });
+    const message = await inbox.methods.message().call();
+
+    assert.notEqual(message, initialString);
+    assert.equal(message, newMessage);
   });
 });
