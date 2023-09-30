2dd42dbacb9ffafd54bd3bafa663d8d1e5dcdf09
Olubo Ifeoluwa <ifeoluwaolubo@gmail.com>
2023-09-30 10:42:22 +0100

    worked on compiling the contract and wrote test for the contract deployment

--- compile.js
+++ compile.js
@@ -2,7 +2,9 @@ const path = require("path");
 const fs = require("fs");
 const solc = require("solc");
 
-const inboxPath = path.resolve(__dirname, "contracts", "inbox.sol");
-const source = fs.readFileSync(inboxPath, "utf-8");
+const inboxPath = path.resolve(__dirname, "contracts", "Inbox.sol");
+const source = fs.readFileSync(inboxPath, "utf8");
 
-module.exports = solc.compile(source, 1).contracts[":Inbox"];
+const contracts = solc.compile(source, 1).contracts;
+
+module.exports = contracts[":Inbox"];
